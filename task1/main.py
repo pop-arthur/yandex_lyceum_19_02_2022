1ff034c4ef36f6b996fc7a26f0e658f628fc4a87
st10-01-16 <artu.200505@gmail.com>
2022-02-19 15:46:31 +0500

    task1

---
+++ task1/main.py
@@ -0,0 +1,13 @@
+from flask import Flask, url_for, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/<title>')
+@app.route('/index/<title>')
+def main_page(title):
+    return render_template("base.html", title=title)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
